f2aaf11ac1a9e3c30d4c9ffa868b86aa179e9e64
BradNicolle <bradnicolle@gmail.com>
2015-03-12 13:23:54 +1030

    Now sends given message from given jid to given jid

--- fb-steg.py
+++ fb-steg.py
@@ -7,6 +7,8 @@ def session_start(event):
     chatbot.send_presence()
     print('Session started')
     chatbot.get_roster()
+    chatbot.send_message(mto=sendto, mbody=msg, mtype='chat')
+    chatbot.disconnect(wait=True)
 
 def message(msg):
     if msg['type'] in ('chat','normal'):
@@ -15,13 +17,15 @@ def message(msg):
 
         msg.reply('Thanks').send()
 
-jid = 'brad.nicolle@chat.facebook.com'
+jid = input('Send from:')
 password = getpass.getpass()
+sendto = input('Send to: ')
+msg = input('Message: ')
 server = ('chat.facebook.com', 5222)
 
 chatbot = sleekxmpp.ClientXMPP(jid,password)
 chatbot.add_event_handler('session_start', session_start)
-chatbot.add_event_handler('message', message)
+#chatbot.add_event_handler('message', message)
 chatbot.auto_reconnect = True
 chatbot.connect(server)
 chatbot.process(block=True)
